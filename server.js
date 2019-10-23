3f236bf3b8332f66df2ebf97ba6cc4db67ece314
Craig Shih <cshih1141@gmail.com>
2019-10-22 21:03:16 -0700

    updated fec proxy id

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const path = require('path');
 
 app.use(express.json());
 
-app.use('/propertyID/:propertyID', express.static(path.join(__dirname, './public')));
+app.use('/:propertyId/propertyID', express.static(path.join(__dirname, './public')));
 
 app.listen(3010);
